ed0b12adfb94201b1f9c0379d48226caf223c93d
osvegn <thomas.prudhomme@epitech.eu>
2023-05-16 06:36:34 +0200

    Add script to build flutter mobile app and flutter web app #1

---
+++ .github/workflows/scripts/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function statusMessage() {
+    if [ $1 -eq 0 ]; then
+        echo $2
+    else
+        echo $3
+        exit 1
+    fi
+}
+
+# Check if one of the arguments is "mobile"
+if [[ "$@" =~ "mobile" ]]; then
+    echo "Building Flutter app"
+    docker build -t zone-o-app:latest . -f Dockerfile.mobile --no-cache
+    statusMessage $? "Flutter app built successfully" "Flutter app build failed"
+fi
+
+# Check if one of the arguments is "web"
+if [[ "$@" =~ "web" ]]; then
+    echo "Building Flutter web"
+    docker build -t zone-o-web:latest . -f Dockerfile.web --no-cache
+    statusMessage $? "Flutter web built successfully" "Flutter web build failed"
+fi
